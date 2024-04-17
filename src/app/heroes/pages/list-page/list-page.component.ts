253dd1a05f1ce719d32408806611eb5bc6e6290e
mikeaude1 <miguelaude@ymail.com>
2024-04-16 20:19:33 -0600

    avance tarjetas de heroes

--- src/app/heroes/pages/list-page/list-page.component.ts
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-list-page',
   templateUrl: './list-page.component.html',
   styles: ``
 })
-export class ListPageComponent {
+export class ListPageComponent implements OnInit {
+  public heroes:Hero[]=[];
 
+  constructor(private heroService:HeroService){}
+
+  ngOnInit(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes=> this.heroes=heroes);
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+  }
 }
---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,3 @@
+export const environments ={
+  baseURL:'https://miguel-aude.com/api'
+}
